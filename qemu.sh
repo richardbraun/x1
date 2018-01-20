3b40c8df13ad7012aedeaa327d3143af205610db
Richard Braun <rbraun@sceen.net>
2018-01-20 03:50:38 +0100

    Fix comments in the qemu.sh script

--- qemu.sh
+++ qemu.sh
@@ -3,8 +3,7 @@
 # Start the QEMU emulator with options doing the following :
 #  - GDB remote access on the local TCP port 1234
 #  - 64MB of physical memory (RAM)
-#  - No video device (automatically sets the first COM port as the console)
-#  - Boot from the generated cdrom image.
+#  - No video device (automatically sets the first serial port as the console)
 #
 # In order to dump all exceptions and interrupts to a log file, you may add
 # the following options :
@@ -12,6 +11,7 @@
 #       -D int_debug.log \
 #
 # Note that these debugging options do not work when KVM is enabled.
+
 qemu-system-i386 \
         -gdb tcp::1234 \
         -m 64 \
